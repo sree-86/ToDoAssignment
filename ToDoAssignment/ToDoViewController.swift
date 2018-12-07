0e7dfb8b9cf725ef1ec391df63a29f3abb003a5f
sree-86 <sriram3rhyme@gmail.com>
2018-12-07 17:18:05 -0500

    TableView Code implemented

--- ToDoAssignment/ToDoViewController.swift
+++ ToDoAssignment/ToDoViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 import CoreData
 
 class ToDoViewController: UITableViewController {
+    
+    var items = [Items]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.name
+        cell.accessoryType = item.completed ? .checkmark : .none
+        return cell
     }
 
 
